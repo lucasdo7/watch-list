c84d4e6c8f427a8205d1d30128a5a822a343fab5
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-12 17:12:52 -0300

    models validations
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, :list, uniqueness: true
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  validates :title, :overview, :rating, presence: true
+  validates :title, :overview, uniqueness: true
 end
