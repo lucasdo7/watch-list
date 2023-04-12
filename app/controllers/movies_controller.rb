b9e84673cea54e7aeadfc74da08fe11619572c62
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-12 17:59:42 -0300

    movie routes, controllers and views
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,31 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render "new", status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
 end
