b8d9416ce9cc3925d05fa02462518b9221db01ad
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-12 18:14:20 -0300

    increased simple_form
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "movies#index"
 
-  resources :movies, only: %i[index show create]
+  resources :movies, only: %i[index show create new]
 end
