bc11770e6831f33d82ab96106197b0c8ccb5e9b7
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-12 18:27:11 -0300

    destroy movies controller and views
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "movies#index"
-
-  resources :movies, only: %i[index show create new]
+  root "lists#index"
 
   resources :lists, only: %i[index show create new]
 end
