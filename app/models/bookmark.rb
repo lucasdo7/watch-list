2a8359bb2765f30364f55d2e41b5349b1d97bd10
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-12 17:31:36 -0300

    associations and validations done
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie, :list, uniqueness: true
   validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'Already in the list' }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,5 @@ class List < ApplicationRecord
   validates :name, uniqueness: true
 
   has_many :bookmarks
-  has_many :movies, through: :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
 end
