465453e33d2f7b2be24cb6e068dff807df977d92
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-12 19:06:26 -0300

    improved bookmarks controllers with private methods
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Bookmark was successfully added.'
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "lists#index"
 
-  resources :lists, only: %i[index show create new]
+  resources :lists, only: %i[index show create new] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
 end
